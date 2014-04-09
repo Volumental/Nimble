36d4a34f31919b986c60452c3920dec1c11b91e4
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-04-09 20:09:05 +0200

    Adding first dll import.

---
+++ Nimble/Native/OpenNI2.cs
@@ -0,0 +1,10 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Nimble.Native
+{
+    internal class OpenNI2
+    {
+        [DllImport("OpenNI2")]
+        public static extern void Initialize();
+    }
+}
