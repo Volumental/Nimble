5c9e9301d867fa51f785488e5a8882d12ee6f090
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-04-22 20:54:15 +0200

    Refactor: Adding disposable base class
    Needed for classes holding unmanged resources

---
+++ Nimble/Disposable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Nimble
+{
+    internal abstract class Disposable: IDisposable
+    {
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                FreeUnmanagedResources();
+            }
+        }
+
+        protected abstract void FreeUnmanagedResources();
+    }
+}
