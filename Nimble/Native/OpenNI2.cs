67e9788d910ed77ce2d8254a80983be8878d960e
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-04-22 21:39:52 +0200

    Adding frame callback functions

--- Nimble/Native/OpenNI2.cs
+++ Nimble/Native/OpenNI2.cs
@@ -38,6 +38,8 @@ namespace Nimble.Native
 	    ONI_SENSOR_DEPTH = 3,
     };
 
+    internal delegate void OniNewFrameCallback(IntPtr streamHandle, IntPtr cookie);
+
     internal class OpenNI2
     {
         // Library functions
@@ -60,10 +62,18 @@ namespace Nimble.Native
         [DllImport("OpenNI2", CallingConvention = CallingConvention.Cdecl)]
         public static extern Status oniDeviceClose(IntPtr deviceHandle);
 
+        // Stream functions
         [DllImport("OpenNI2", CallingConvention = CallingConvention.Cdecl)]
         public static extern Status oniDeviceCreateStream(IntPtr deviceHandle, OniSensorType sensorType, out IntPtr streamHandle);
 
         [DllImport("OpenNI2", CallingConvention = CallingConvention.Cdecl)]
         public static extern void oniStreamDestroy(IntPtr streamHandle);
+
+        [DllImport("OpenNI2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern Status oniStreamRegisterNewFrameCallback(IntPtr streamHandle, OniNewFrameCallback handler, IntPtr cookie, out IntPtr callbackHandle);
+
+        [DllImport("OpenNI2", CallingConvention = CallingConvention.Cdecl)]
+        public static extern void oniStreamUnregisterNewFrameCallback(IntPtr streamHandle, IntPtr callbackHandle);
+
     }
 }
