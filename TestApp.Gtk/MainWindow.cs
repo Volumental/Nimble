6c7821dc9459dcdb7f470fe2f026fba3350a4cc4
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-05-01 16:29:54 +0200

    Gtk project now lists devices. :-)

--- TestApp.Gtk/MainWindow.cs
+++ TestApp.Gtk/MainWindow.cs
@@ -1,11 +1,22 @@
 ﻿using System;
 using Gtk;
+using Nimble;
 
 public partial class MainWindow: Gtk.Window
 {
 	public MainWindow () : base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
+
+		var openni = new OpenNI();
+		openni.Initialize ();
+		Console.WriteLine ("Devices");
+		foreach (var device in openni.Devices)
+		{
+			Console.WriteLine (device.Name);
+		}
+		openni.Shutdown ();
+
 	}
 
 	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
