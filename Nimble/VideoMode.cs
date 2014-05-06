b1c3f529b6cbbcf94745ec9517e67745015e55e4
Samuel Carlsson <samuel.carlsson@gmail.com>
2014-05-06 20:46:39 +0200

    Refactor: Moving out class to its own file.

---
+++ Nimble/VideoMode.cs
@@ -0,0 +1,30 @@
+﻿using Nimble.Native;
+
+namespace Nimble
+{
+    public class VideoMode
+    {
+        private readonly int _width;
+        private readonly int _height;
+        private readonly int _fps;
+        private PixelFormat _pixelFormat;
+
+        public VideoMode(int width, int height, int fps, PixelFormat pixelFormat)
+        {
+            _width = width;
+            _height = height;
+            _fps = fps;
+            _pixelFormat = pixelFormat;
+        }
+
+        public int Width { get { return _width; } }
+        public int Height { get { return _height; } }
+        public int Fps { get { return _fps; } }
+        public PixelFormat PixelFormat { get { return _pixelFormat; } }
+
+        internal OniVideoMode ToRaw()
+        {
+            return new OniVideoMode() { resolutionX = _width, resolutionY = _height, fps = _fps, pixelFormat = _pixelFormat.ToRaw() };
+        }
+    }
+}
